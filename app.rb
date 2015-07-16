f9e66b6ed9af5d82d97331fcc380b0fd46c11ee1
pat <psmolen@gmail.com>
2015-07-16 11:25:13 -0500

    update json format

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/' do
   client = Instagram.client(client_id: ENV["INSTAGRAM_CLIENT_ID"])
   hotspots = []
   for media_item in client.media_search(params[:lat],params[:lon])
-    hotspots << {id: media_item[:id], imageURL: media_item[:images][:low_resolution][:url], anchor: {geolocation: {lat: media_item[:location][:latitude], lng: media_item[:location][:longitude]}}}
+    hotspots << {id: media_item[:id], icon: {url: media_item[:images][:low_resolution][:url]}, anchor: {geolocation: {lat: media_item[:location][:latitude], lon: media_item[:location][:longitude]}}}
   end
-  hotspots.to_json
+  {hotspots: hotspots}.to_json
 end
