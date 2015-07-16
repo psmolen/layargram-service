800752ed054a650afbfcde52c4ef5445d4c58a84
pat <psmolen@gmail.com>
2015-07-16 11:08:05 -0500

    use params for lat/lng

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
   content_type :json
   client = Instagram.client(client_id: ENV["INSTAGRAM_CLIENT_ID"])
   hotspots = []
-  for media_item in client.media_search("37.7808851","-122.3948632")
+  for media_item in client.media_search(params[:lat],params[:lng])
     hotspots << {id: media_item[:id], imageURL: media_item[:images][:low_resolution][:url], anchor: {geolocation: {lat: media_item[:location][:latitude], lng: media_item[:location][:longitude]}}}
   end
   hotspots.to_json
