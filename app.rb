166541a090525cc83d7567a7d314e867f0453520
pat <psmolen@gmail.com>
2015-07-16 17:18:44 -0500

    refactor hotspots

--- app.rb
+++ app.rb
@@ -1,12 +1,7 @@
 require 'sinatra'
-require 'instagram'
+require './models/hotspots.rb'
 
 get '/' do
   content_type :json
-  client = Instagram.client(client_id: ENV["INSTAGRAM_CLIENT_ID"])
-  hotspots = []
-  for media_item in client.media_search(params[:lat],params[:lon])
-    hotspots << {id: media_item[:id], icon: {url: media_item[:images][:low_resolution][:url]}, anchor: {geolocation: {lat: media_item[:location][:latitude], lon: media_item[:location][:longitude]}}, text: {title: "", description: media_item[:caption] ? media_item[:caption][:text] : ""}, imageURL: media_item[:images][:low_resolution][:url], actions: [{uri: media_item[:link], label: "View", contentType: "text/html"}] }
-  end
-  {hotspots: hotspots, layer: params[:layerName], errorCode: 0, errorString: :ok}.to_json
+  {hotspots: Hotspots.instagram_hotspots(params[:lat], params[:lon]), layer: params[:layerName], errorCode: 0, errorString: :ok}.to_json
 end
---
+++ models/hotspots.rb
@@ -0,0 +1,40 @@
+require 'instagram'
+
+class Hotspots
+
+  def self.instagram_hotspots(lat,lon)
+    client = Instagram.client(client_id: ENV["INSTAGRAM_CLIENT_ID"])
+    
+    hotspots = client.media_search(lat,lon).map do |media_item|
+      {
+        id: media_item[:id],
+        icon: 
+        {
+          url: media_item[:images][:low_resolution][:url]
+        },
+        anchor: 
+        {
+          geolocation: 
+          {
+            lat: media_item[:location][:latitude], 
+            lon: media_item[:location][:longitude]
+          }
+        }, 
+        text: 
+        {
+          title: "", 
+          description: media_item[:caption] ? media_item[:caption][:text] : ""
+        }, 
+        imageURL: media_item[:images][:low_resolution][:url], 
+        actions: 
+        [{
+          uri: media_item[:link], 
+          label: "View", 
+          contentType: "text/html"
+        }]
+      }
+    end
+
+  end
+
+end
