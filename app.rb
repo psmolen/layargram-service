7896554bdd69bd82acefb585c6a87faba336a1a3
pat <psmolen@gmail.com>
2015-07-16 15:22:39 -0500

    use false instead of :false

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/' do
   client = Instagram.client(client_id: ENV["INSTAGRAM_CLIENT_ID"])
   hotspots = []
   for media_item in client.media_search(params[:lat],params[:lon])
-    hotspots << {id: media_item[:id], icon: {url: media_item[:images][:low_resolution][:url]}, anchor: {geolocation: {lat: media_item[:location][:latitude], lon: media_item[:location][:longitude]}}, text: {title: "", description: media_item[:caption] ? media_item[:caption][:text] : ""}, imageURL: media_item[:images][:low_resolution][:url], showBiwOnClick: :false, actions: [{uri: media_item[:link], label: "View", contentType: "text/html"}] }
+    hotspots << {id: media_item[:id], icon: {url: media_item[:images][:low_resolution][:url]}, anchor: {geolocation: {lat: media_item[:location][:latitude], lon: media_item[:location][:longitude]}}, text: {title: "", description: media_item[:caption] ? media_item[:caption][:text] : ""}, imageURL: media_item[:images][:low_resolution][:url], showBiwOnClick: false, actions: [{uri: media_item[:link], label: "View", contentType: "text/html"}] }
   end
   {hotspots: hotspots, layer: params[:layerName], errorCode: 0, errorString: :ok}.to_json
 end
